f85f586ddf6002966c288115218d5547046c08ac
Garrion Martin <unit678@gmail.com>
2018-12-09 18:12:50 -0500

    connections

--- config/connection.js
+++ config/connection.js
@@ -29,13 +29,13 @@ if (process.env.JAWSDB_URL) {
 
 
 // Make connection
-connection.connect  ();      //(function(err){
-    //if (err) {
-      //  console.error("error connecting: " + err.stack);
-      // return;
-   // }
-    //console.log("connected as id: " + connection.threadId);
-//});
+connection.connect(function(err){
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+   }
+    console.log("connected as id: " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
